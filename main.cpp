cfcda7fbdb281c3c8e3e33cf2c7c00154efcf64b
raudzius <raudziusmykolas@gmail.com>
2026-06-17 21:13:46 +0300

    feat: extract movie data from file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+struct Movie {
+    int id;
+    string title;
+    int year;
+    double rating;
+};
+
+vector<Movie> movies;
+
+void loadFromFile() {
+    ifstream moviesFile("movies.txt");
+    if (moviesFile.fail()) {
+        cout << "Failed to open file!" << endl;
+        return;
+    }
+
+    string line;
+
+    while (getline(moviesFile, line)) {
+        stringstream stringStreamLine(line);
+        Movie movie;
+        string temporaryString;
+
+        getline(stringStreamLine, temporaryString, ';');
+        movie.id = stoi(temporaryString);
+
+        getline(stringStreamLine, temporaryString, ';');
+        movie.title = temporaryString;
+
+        getline(stringStreamLine, temporaryString, ';');
+        movie.year = stoi(temporaryString);
+
+        getline(stringStreamLine, temporaryString, ';');
+        movie.rating = stod(temporaryString);
+
+        movies.push_back(movie);
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    loadFromFile();
     return 0;
 }
